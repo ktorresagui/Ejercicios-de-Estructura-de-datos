daac27f242631ba333019fa30ecfd7fcf303bbda
ktorresagui <al03022345@tecmilenio.mx>
2025-08-14 11:20:09 -0600

    Create Vehiculo.java

---
+++ dia3/Vehiculo.java
@@ -0,0 +1,37 @@
+package principal;
+
+/**
+ *
+ * @author kst01
+ */
+public abstract class Vehiculo {
+    private String marca;
+    private String modelo;
+    private int anio;
+    private double precio;
+
+    public Vehiculo(String marca, String modelo, int anio, double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.precio = precio;
+    }
+
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    public int getAnio() { return anio; }
+    public void setAnio(int anio) { this.anio = anio; }
+
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+
+    public abstract double calcularDescuento();
+
+    public void mostrarInfo() {
+        System.out.println(marca + " " + modelo + " (" + anio + ") - $" + precio);
+    }
+}
